838942e710d8c552ca08f13bf95658b69cb796ed
Otega Otite <otiteotega@gmail.com>
2026-01-13 23:55:26 +0100

    create first route using react router

--- phone_book/src/App.jsx
+++ phone_book/src/App.jsx
@@ -1,12 +1,10 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 
-const App = () => {
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <div>Hello world!</div>
+  }
+]);
 
-  return (
-    <RouterProvider router={router} />
-  )
-}
-export default App;
+export default router;
