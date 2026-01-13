245b54f60b1327e00eb6a514eaa0fbc0e89d1d18
Otega Otite <otiteotega@gmail.com>
2026-01-13 23:38:10 +0100

    Scaffold phone book and installed necessary dependencies

---
+++ phone_book/src/App.jsx
@@ -0,0 +1,12 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+
+const App = () => {
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+export default App;
